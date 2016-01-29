0f3b0a15f00ecb03df086abbb0969ada59854658
James Echmalian <ech@ech.net>
2016-01-29 14:42:41 -0500

    add basic interactivity

--- background.js
+++ background.js
@@ -1,8 +1,29 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
+var statusWindow;
+var displayWindow;
+
+// Log messages go to the background page view.
+console.log("background script running");
+
+// How do you access page scripts from here?
+// How do you make objects available to page scripts from here?
+
+function createStatusWindow(launchData) {
+  chrome.app.window.create('status.html', {
     'outerBounds': {
       'width': 400,
-      'height': 500
+      'height': 500,
+      'left': 100,
+      'top': 100
     }
+  }, function(win) {
+    win.contentWindow.launchData = launchData;
+    statusWindow = win;
   });
+}
+
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  console.log("application launched");
+  createStatusWindow(launchData);
 });
+
+console.log("background script finished");
